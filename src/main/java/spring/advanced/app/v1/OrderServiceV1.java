94554bc703fd596417fec3461ecc1e43bb4b64f1
jyh4358 <jyh4358@gmail.com>
2022-08-31 17:29:48 +0900

    feat: 로그추적기 v1

---
+++ src/main/java/spring/advanced/app/v1/OrderServiceV1.java
@@ -0,0 +1,29 @@
+package spring.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.hellotrace.HelloTraceV1;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+
+        TraceStatus status = null;
+        orderRepository.save(itemId);
+
+        try {
+            status = trace.begin("OrderService.orderitem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
