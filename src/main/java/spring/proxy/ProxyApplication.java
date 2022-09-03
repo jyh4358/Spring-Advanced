fa413ed8724f45dce4db19854e2a13f2ad6c23d6
jyh4358 <jyh4358@gmail.com>
2022-09-03 18:01:22 +0900

    feat: bean 등록 3가지 방법

---
+++ src/main/java/spring/proxy/ProxyApplication.java
@@ -0,0 +1,18 @@
+package spring.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import spring.proxy.config.AppV1Config;
+import spring.proxy.config.AppV2Config;
+
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
+@SpringBootApplication(scanBasePackages = "spring.proxy.app")
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+}
