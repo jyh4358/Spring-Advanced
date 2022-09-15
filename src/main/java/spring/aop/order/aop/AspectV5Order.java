38cf26a1d034c850f9115b4216d3806b71c14fae
jyh4358 <jyh4358@gmail.com>
2022-09-15 17:41:56 +0900

    feat: Spring AOP 구현 - 순서 지정

---
+++ src/main/java/spring/aop/order/aop/AspectV5Order.java
@@ -0,0 +1,55 @@
+package spring.aop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+
+@Slf4j
+//@Aspect
+public class AspectV5Order {
+
+    // 포인트컷을 따로 분리할 수 있다.
+    @Pointcut("execution(* spring.aop.order..*(..))")
+    private void allOrder(){}   // pointcut signature
+
+    // 클래스 이름 패턴이 *Service 인 포인트컷
+    @Pointcut("execution(* *..*Service.*(..))")
+    private void allService(){}
+
+    @Aspect
+    @Order(2)
+    public static class LogAspect {
+        @Around("allOrder()")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{
+            log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+            return joinPoint.proceed();
+        }
+    }
+
+    @Aspect
+    @Order(1)
+    public static class TxAspect{
+        @Around("allOrder() && allService()")
+        public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+
+            try {
+                log.info("[트랜잭션 시작] {}", joinPoint.getSignature());
+                Object result = joinPoint.proceed();
+                log.info("[트랜잭션 커밋] {}", joinPoint.getSignature());
+                return result;
+            } catch (Exception e) {
+                log.info("[트랜잭션 롤백] {}", joinPoint.getSignature());
+                throw e;
+            } finally {
+                log.info("[리소스 릴리즈] {}", joinPoint.getSignature());
+            }
+        }
+    }
+
+
+
+
+}
